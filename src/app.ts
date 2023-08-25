ae4cc881c5abade6553646f668367fb96932ff70
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-08-25 17:59:31 -0500

    finaliza ejercicio para enviar

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,42 @@ app.get('/api', (_, res: Response) => {
     res.send('Hello World!');
 });
 
+app.get('/api/tasks/', (_: Request, res: Response) => {
+    const allTasks = tasks;
+
+    res.status(200).json(allTasks);
+});
+
+app.get('/api/tasks/:id', (req: Request, res: Response) => {
+    const { id } = req.params;
+    const singleTask = tasks.find((task) => task.id.toString() === id);
+
+    res.status(200).json(singleTask);
+});
+
+app.post('/api/tasks/', (req: Request, res: Response) => {
+    const body = req.body;
+    tasks.push(body);
+
+    res.status(200).json(tasks);
+});
+
+app.put('/api/tasks/:id', (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { todo } = req.body;
+
+    tasks.map((task) => { if (task.id.toString() === id) { task.todo = todo } })
+
+    res.status(200).json(tasks);
+});
+
+app.delete('/api/tasks/:id', (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    tasks.splice(Number(id) - 1);
+
+    res.status(200).json(tasks);
+});
 
 app.listen(port, () => {
     console.log(`Server Running Up on port ${port}`);
